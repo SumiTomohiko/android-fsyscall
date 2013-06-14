1e2d193c3f8a7767278b6eba8a8e608bd9c17224
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-06-14 17:39:51 +0900

    Enhanced to run the nexec client.
    This version changes the thread policy[1] of Android. This is not a good
    way.
    
    [1] http://developer.android.com/reference/android/os/StrictMode.html

--- src/jp/gr/java_conf/neko_daisuki/android/nexec/client/MainActivity.java
+++ src/jp/gr/java_conf/neko_daisuki/android/nexec/client/MainActivity.java
@@ -1,10 +1,40 @@
 package jp.gr.java_conf.neko_daisuki.android.nexec.client;
 
+import java.io.IOException;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+import jp.gr.java_conf.neko_daisuki.nexec.client.NexecClient;
 
 public class MainActivity extends Activity {
+
+    private class RunButtonOnClickListener implements View.OnClickListener {
+
+        public void onClick(View view) {
+            NexecClient nexec = new NexecClient();
+            String host = mHostEdit.getText().toString();
+            int port = Integer.parseInt(mPortEdit.getText().toString());
+            String[] args = new String[] { mArgsEdit.getText().toString() };
+            try {
+                nexec.run(host, port, args);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private EditText mHostEdit;
+    private EditText mPortEdit;
+    private EditText mArgsEdit;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -17,6 +47,17 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View runButton = findViewById(R.id.run_button);
+        runButton.setOnClickListener(new RunButtonOnClickListener());
+
+        mHostEdit = (EditText)findViewById(R.id.host_edit);
+        mPortEdit = (EditText)findViewById(R.id.port_edit);
+        mArgsEdit = (EditText)findViewById(R.id.args_edit);
+    }
+
+    static {
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
     }
 }
 
